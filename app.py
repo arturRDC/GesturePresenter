a8c527d7d2000e3caed198bb2f50c67c8bcc1639
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-07-29 10:06:27 -0300

    Identify index finger landmark

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ drawing_utils = mp.solutions.drawing_utils
 hand_detector = mp.solutions.hands.Hands()
 while True:
   _, frame = cap.read()
+  frame = cv2.flip(frame,1)
+  f_height, f_width, _ = frame.shape
   rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
   detection = hand_detector.process(rgb_frame)
   hands = detection.multi_hand_landmarks
@@ -15,6 +17,14 @@ while True:
   if hands:
     for hand in hands:
       drawing_utils.draw_landmarks(frame, hand)
+      landmarks = hand.landmark
+      for id, landmark in enumerate(landmarks):
+        x = int(landmark.x * f_width)
+        y = int(landmark.y * f_height)
+        print(x,y)
+        if id == 8:
+          cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
+
 
   cv2.imshow('Gesture presenter', frame)
   key = cv2.waitKey(10)
